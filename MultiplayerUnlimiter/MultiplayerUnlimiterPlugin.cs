03fa0e759d18888db86b6bd8bcb3c4f4f74a04a1
willis81808 <willis81808@gmail.com>
2023-03-04 23:35:54 -0700

    replaced standalone settings panel with Mache window integration

--- MultiplayerUnlimiter/MultiplayerUnlimiterPlugin.cs
+++ MultiplayerUnlimiter/MultiplayerUnlimiterPlugin.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
-using Mache;
 using Sons.Multiplayer;
 using UnityEngine;
 using UnityEngine.UI;
 using UniverseLib;
 using UniverseLib.UI;
+using Mache;
+using Mache.UI;
 
 namespace MultiplayerUnlimiter
 {
@@ -22,7 +23,7 @@ namespace MultiplayerUnlimiter
     {
         public const string ModId = "com.willis.sotf.mpunlimiter";
         public const string ModName = "Multiplayer Unlimiter";
-        public const string Version = "0.0.1";
+        public const string Version = "0.1.0";
 
         internal static MultiplayerUnlimiterPlugin Instance { get; private set; }
 
@@ -36,8 +37,6 @@ namespace MultiplayerUnlimiter
     public class Unlimiter : MonoBehaviour
     {
         internal static Unlimiter Instance { get; private set; }
-
-        private static UnlimiterMenu Menu { get; set; }
 
         private static ConfigEntry<int> _maximumPlayers;
         internal static int MaximumPlayers
@@ -67,20 +66,34 @@ namespace MultiplayerUnlimiter
 
         private void Start()
         {
-            Mache.Mache.RegisterMod(() =>
+            Mache.Mache.RegisterMod(() => new ModDetails
             {
-                var uiBase = UniversalUI.RegisterUI(MultiplayerUnlimiterPlugin.ModId, OnMenuUpdate);
-                Menu = new UnlimiterMenu(uiBase);
-
-                return new ModDetails
-                {
-                    Id = MultiplayerUnlimiterPlugin.ModId,
-                    Version = MultiplayerUnlimiterPlugin.Version,
-                    Name = MultiplayerUnlimiterPlugin.ModName,
-                    Description = "Customize the maximum player limit!",
-                    OnMenuShow = ShowMenu
-                };
+                Id = MultiplayerUnlimiterPlugin.ModId,
+                Version = MultiplayerUnlimiterPlugin.Version,
+                Name = MultiplayerUnlimiterPlugin.ModName,
+                Description = "Customize the maximum player limit!",
+                OnFinishedCreating = CreateMenu,
             });
+        }
+
+        private void CreateMenu(GameObject parent)
+        {
+            MenuPanel.Builder()
+                .AddComponent(new SliderComponent
+                {
+                    StartValue = MaximumPlayers,
+                    MinValue = 6,
+                    MaxValue = 25,
+                    WholeNumbers = true,
+                    Name = "Maximum Players",
+                    OnValueChanged = OnMaxPlayersValueChanged
+                })
+                .BuildToTarget(parent);
+        }
+
+        private void OnMaxPlayersValueChanged(SliderComponent slider, float val)
+        {
+            MaximumPlayers = (int)val;
         }
 
         private void Update()
@@ -92,53 +105,6 @@ namespace MultiplayerUnlimiter
 
             lobbyManager.SetMemberLimit(MaximumPlayers);
             initialApplied = true;
-        }
-
-        private void ShowMenu()
-        {
-            Menu.SetActive(true);
-        }
-
-        private void OnMenuUpdate() { }
-    }
-
-    public class UnlimiterMenu : UniverseLib.UI.Panels.PanelBase
-    {
-        public override string Name => MultiplayerUnlimiterPlugin.ModName;
-        public override int MinWidth => 750;
-        public override int MinHeight => 85;
-        public override Vector2 DefaultAnchorMin => Vector2.zero;
-        public override Vector2 DefaultAnchorMax => Vector2.zero;
-        public override bool CanDragAndResize => true;
-
-        private Text label;
-
-        public UnlimiterMenu(UIBase owner) : base(owner) { }
-
-        protected override void ConstructPanelContent()
-        {
-            var container = UIFactory.CreateHorizontalGroup(ContentRoot, "max_players_row", true, false, true, true, spacing: 10, padding: new Vector4(10, 10, 10, 10));
-
-            label = UIFactory.CreateLabel(container, "max_players_label", $"Maximum Players ({Unlimiter.MaximumPlayers})", TextAnchor.MiddleRight);
-            UIFactory.SetLayoutElement(label.gameObject, minWidth: 85, minHeight: 30, flexibleWidth: 0);
-
-            var sliderObj = UIFactory.CreateSlider(container, "max_players_slider", out var slider);
-            UIFactory.SetLayoutElement(sliderObj, minHeight: 30, flexibleWidth: 9999);
-
-            slider.minValue = 8;
-            slider.maxValue = 20;
-            slider.wholeNumbers = true;
-            slider.value = Unlimiter.MaximumPlayers;
-
-            slider.onValueChanged.AddListener(OnMaxPlayersValueChanged);
-
-            SetActive(false);
-        }
-
-        private void OnMaxPlayersValueChanged(float val)
-        {
-            label.text = $"Maximum Players ({val})";
-            Unlimiter.MaximumPlayers = (int)val;
         }
     }
 }
